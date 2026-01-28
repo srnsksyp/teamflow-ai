2195ec458dc69b3a64b5def4ab95e6b2a9ae522a
Saransh Kashyap <saranshk0511@gmail.com>
2026-01-28 23:32:57 +0530

    Implement security middleware for heavy write and read operations.

---
+++ app/middlewares/arcjet/heavy-write.ts
@@ -0,0 +1,37 @@
+import arcjet, { slidingWindow } from "@/lib/arcjet";
+import { base } from "../base";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs";
+
+const buildStandardAj = () =>
+  arcjet.withRule(
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 2,
+    }),
+  );
+
+export const heavyWriteSecurityMiddleware = base
+  .$context<{
+    request: Request;
+    user: KindeUser<Record<string, unknown>>;
+  }>()
+  .middleware(async ({ context, next, errors }) => {
+    const decision = await buildStandardAj().protect(context.request, {
+      userId: context.user.id,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        throw errors.RATE_LIMITED({
+          message: "Too many impactual changes. Please slow down",
+        });
+      }
+
+      throw errors.FORBIDDEN({
+        message: "Request Blocked!",
+      });
+    }
+
+    return next();
+  });
